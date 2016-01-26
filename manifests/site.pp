47609b2ba0fbae27c11dec1ff14187a6a17714e7
hadoop <hadoop@192-168-1-109.tpgi.com.au>
2016-01-26 15:37:13 +1100

    more

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,26 +1,44 @@
 node default {
-  file { "/tmp/hello7" :
-    content => "hello bogotobogo.com"
-  }
-   package { 'httpd':
-        ensure => 'installed'
-       }
+  file { "/tmp/hello7": content => "hello bogotobogo.com" }
+
+  package { 'httpd': ensure => 'installed' }
 }
 
 node 'ec2-52-91-67-250.compute-1.amazonaws.com' {
   include sudo
   include apache
-#  include mysql
-#  include postfix
+  #  include mysql
+  #  include postfix
   include ssh
-  file { "/tmp/hello3" :
-    content => "hello bogotobogo.com"
+
+  file { "/tmp/hello3": content => "hello bogotobogo.com" }
+
+  service { 'httpd':
+    ensure  => running,
+    require => Package['httpd'],
   }
+
+  package { 'httpd': ensure => installed, }
+
+  file { '/etc/httpd/conf.d/cookbook.conf':
+    content => "<VirtualHost *:80>\nServername
+             cookbook\nDocumentRoot
+             /var/www/cookbook\n</VirtualHost>\n",
+    require => Package['httpd'],
+    notify  => Service['httpd'],
+  }
+
+  file { '/var/www/cookbook': ensure => directory, }
+
+  file { '/var/www/cookbook/index.html':
+    content => "<html><h1>Hello World!</h1></html>\n",
+    require => File['/var/www/cookbook'],
+  }
+
 }
 
-#node 'ec2-52-91-203-156.compute-1.amazonaws.com' {
-#	class { '::sudo':
+# node 'ec2-52-91-203-156.compute-1.amazonaws.com' {
+# class { '::sudo':
 #     users => ['tom', 'jerry'],
 #  }
 #}
-
